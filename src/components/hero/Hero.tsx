902743d9cc29de5dcee35c2ac758e0d9657449cb
InterMaus1154 <amarco1154@gmail.com>
2024-12-08 17:25:44 +0000

    hero update

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,7 +5,10 @@ const Hero: FC = () => {
     return (
         <section className={"hero-section page-section full-width"}>
             <div className="full-width-wrapper">
-
+                <div className="hero-title-wrapper">
+                    <h2>Mark Kiss</h2>
+                    <h3>Web Developer. WorldSkills International Lyon 2024 Competitor.</h3>
+                </div>
             </div>
         </section>
     );
