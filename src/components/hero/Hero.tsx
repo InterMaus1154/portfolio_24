1c60acd699bcd6af0ac41228a2c7080db8a70c5b
intermaus1154 <amarco1154@gmail.com>
2025-05-05 09:05:31 +0100

    Edit hero text

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,7 +8,7 @@ const Hero: FC = () => {
                 <div className="hero-title-wrapper">
                     <h2>Márk Kiss</h2>
                     <h3>Web Developer.</h3>
-                    <h3>WorldSkills International Lyon 2024 Competitor.</h3>
+                    <h3>WorldSkills International Lyon 2024: Medallion of Excellence</h3>
                 </div>
             </div>
         </section>
